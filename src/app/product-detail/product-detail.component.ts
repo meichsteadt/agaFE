2765947baac1b7bfd98eab503afd08c87872073f
Matt Eichsteadt <matteichsteadt@gmail.com>
2017-09-11 00:39:56 -0700

    complete products-detail page

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -14,6 +14,7 @@ export class ProductDetailComponent implements OnInit {
   product: Product;
   productId: number = null;
   productItems: ProductItem[] = [];
+  total: number;
   constructor(private route: ActivatedRoute, private http: HttpClient) { }
 
   ngOnInit() {
@@ -22,9 +23,41 @@ export class ProductDetailComponent implements OnInit {
     });
     this.http.get("http://localhost:3000/products/" + this.productId).subscribe(i => this.product = new Product(i["product"]["id"], i["product"]["category"], i["product"]["description"], i["product"]["name"], i["product"]["number"], i["product"]["image"]));
     this.http.get("http://localhost:3000/products/" + this.productId).subscribe(i => i["product_items"].forEach(item => {
-      let productItem = new ProductItem(item["description"], item["dimensions"], item["id"], item["number"], item["price"])
+      let productItem = new ProductItem(item["description"], item["dimensions"], item["id"], item["number"], item["price"], 0)
       this.productItems.push(productItem)
     }));
+    this.setTotal();
   }
 
+  dropDown(event, item) {
+    let element = event.srcElement.parentElement.children[1];
+    element.className = "active";
+
+    function clickEvent() {
+      element.className = "";
+      document.body.removeEventListener("click", clickEvent);
+    }
+
+    setTimeout(function() {
+      document.body.addEventListener("click", clickEvent);
+    }, 0)
+
+  }
+
+  selectNumber(event, item) {
+    let value = event.srcElement.value;
+    item.numberSelected = value;
+    event.srcElement.parentElement.className = "";
+    this.setTotal();
+  }
+
+  setTotal() {
+    let sum = 0;
+    if (this.productItems.length > 0) {
+      this.productItems.forEach(function(item) {
+        sum += item.price * item.numberSelected;
+      })
+    }
+    this.total = sum;
+  }
 }
--- src/app/product-item.model.ts
+++ src/app/product-item.model.ts
@@ -1,5 +1,5 @@
 export class ProductItem {
-  constructor(public description: string, public dimensions: string, public id: number, public number: string, public price: number) {
-    
+  constructor(public description: string, public dimensions: string, public id: number, public number: string, public price: number, public numberSelected: number) {
+
   }
 }
